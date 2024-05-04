8007d7ec1bb46a52795d6e5baed19667f60ef829
SHRISHAIL CHANDRASHEKHAR WALI <114606779+shrishailwali@users.noreply.github.com>
2024-05-04 17:54:05 +0000

    DataType Summary

---
+++ JavascriptBasic.js/04_DataTypeInterview.js
@@ -0,0 +1,28 @@
+// Permeative and non-premetive datatypes
+
+//Premetive Datatype (7 types): String, Number, Boolean, Null, undefined, Symbol, BigInt
+
+// Javascript is a Dynamic Typed language Because Typed checked during the code Execute 
+
+
+// console.log('hi')
+const score = 100;
+const scoreValue = 100.3
+
+let userEmail;
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+// console.table(id === anotherId);
+
+// Reference Type (Non-premetive datatypes): Array, Objects, Functions
+
+const heros= ['Shaktian','shri', 'sachin'];
+let  muobj ={
+    name: "SHrishail",
+    age:25,
+}
+
+const myFunction = function(){
+    console.log("Function")
+}
