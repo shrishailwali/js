fde33a60a34c7fced3fef584b98ad7858c8d99d8
SHRISHAIL CHANDRASHEKHAR WALI <114606779+shrishailwali@users.noreply.github.com>
2024-05-04 06:28:33 +0000

    DataTypes

---
+++ JavascriptBasic.js/01_Basic.js
@@ -0,0 +1,11 @@
+console.log("shrishail")
+const accountId = 12324
+let accountEmail = "shrishail@gmail.com"
+var accountPassword = "12345"
+accountCity ="Vijayapur"
+
+console.table([accountId,accountEmail,accountPassword,accountCity])
+/*
+prefers not to use var
+because of issues in the block scope and functionally Scope
+*/
---
+++ JavascriptBasic.js/02_dataType.js
@@ -0,0 +1,23 @@
+"use strict"; //treat all js code as newer versions
+
+// alert (3+3) //we are using node.js, not browsers 
+//Code readability should be high
+//tc39.es
+//Mozilla MDN JavaScript
+
+// Permeative DataType
+// 1) Number => 2 to power 53
+// 2) bigint
+// 3) string => ""
+// 4) boolean => true/false
+// 5) null => standalone value (is a object)
+// 6) unrefined =>
+// 7) symbol => unique
+
+
+// Object DataType
+console.log(typeof null); //object 
+
+console.log(typeof undefined) // undefined 
+
+
