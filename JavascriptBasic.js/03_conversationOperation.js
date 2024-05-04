bde8f70a15e1793e8de0ecb7707711c44934fb56
SHRISHAIL CHANDRASHEKHAR WALI <114606779+shrishailwali@users.noreply.github.com>
2024-05-04 08:21:52 +0000

    Conversation and Operation

---
+++ JavascriptBasic.js/03_conversationOperation.js
@@ -0,0 +1,33 @@
+let score = "33"
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber)
+
+// "33" => 33
+// "33abc" => NaN
+// true =>1; false => 0
+
+let isLoggedIn ="Shri";
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+//1 => true; 0=> false
+// "" => fasle
+//"shri" => true
+
+//*************************************Operations****************************************
+let value = 4
+let negValue = -value
+// console.log(negValue);
+
+let str1 = 'Hello'
+let str2 = " Shrishail"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2); //12
+// console.log(1 + "2"); //12
+// console.log("1" + 2 + 2); //122
+// console.log(1 + 2 + "2"); //32
+
