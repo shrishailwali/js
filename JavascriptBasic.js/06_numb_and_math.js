ee0ca54c22f71ea4d6421bcf0aead72f9f2e2eb8
SHRISHAIL CHANDRASHEKHAR WALI <114606779+shrishailwali@users.noreply.github.com>
2024-05-05 04:50:15 +0000

    Number and Math

---
+++ JavascriptBasic.js/06_numb_and_math.js
@@ -0,0 +1,26 @@
+const score = 400;
+// const balance = new Number(100)
+// console.log(balance);   // specifically will say this is number => [Number: 100]
+// console.log(score); // => 400
+// console.log(balance.toFixed(2)); // => 100.00   //precession valus or decimal values
+
+const hundred =10000000
+// console.log(hundred.toLocaleString('en-IN'));
+
+
+//*********************************Maths ********************************
+// console.log(Math) //object [Math] {}
+
+// console.log(Math.abs(-4));  // sign will change to + positive => 4
+// console.log(Math.round(2.3434))    // 2
+// console.log(Math.ceil(4.2))  // 5
+// console.log(Math.floor(5.3))   // 5
+
+// console.log(Math.random());
+// console.log(Math.floor(Math.random()* 10)+1);
+
+
+const min = 10
+const max =20
+
+console.log(Math.floor(Math.random() * (max - min + 1))+min)
