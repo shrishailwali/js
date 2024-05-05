9621033ccebd7bf0cfbe4b39243792e2fdb068d1
SHRISHAIL CHANDRASHEKHAR WALI <114606779+shrishailwali@users.noreply.github.com>
2024-05-05 06:51:11 +0000

    Dates

---
+++ JavascriptBasic.js/07_date.js
@@ -0,0 +1,14 @@
+// Dates
+
+let myDate = new Date()   // object
+// console.log(myDate);
+// console.log(myDate.toDateString())
+// console.log(myDate.toLocaleString())
+
+let myCreatedDate = new Date(2023, 0, 23)
+// console.log(myCreatedDate.toDateString())
+
+// timestamp
+let myTimeStamp = Date.now()
+console.log(myTimeStamp)
+console.log(myCreatedDate.getTime())
