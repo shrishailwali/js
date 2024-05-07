d69dbea600339ca2ad9e7a4e76b46b1057901582
SHRISHAIL CHANDRASHEKHAR WALI <114606779+shrishailwali@users.noreply.github.com>
2024-05-07 15:36:27 +0000

    controlflow

---
+++ controlFlow/01_control.js
@@ -0,0 +1,33 @@
+// if (true){
+
+// }
+
+// if (2=="2"){   // here we are comparing int with string so we need to use strict equal 
+//     console.log(`execute `)
+// }
+
+if(2==="2"){
+    console.log("execute111")
+}
+
+const score = 200;
+if (score > 100){
+    let power = 'fly';
+    console.log(`${power}`)
+}
+
+// console.log(`${power}`)  //  not able to  access the power outside the if scope 
+
+balance = 1000
+// if (balance > 500) console.log('test'), console.log("test22"); // this is bad practice to write code
+
+if (balance < 500){
+    console.log("1")
+} else if(balance < 900){
+    console.log('2')
+}else{
+    console.log('3')
+}
+
+
+
