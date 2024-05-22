233b5db1ab21461651e2bfbc566b640d056dae33
SHRISHAIL CHANDRASHEKHAR WALI <114606779+shrishailwali@users.noreply.github.com>
2024-05-22 18:14:38 +0000

    SetInterval

--- JsAsync/script.js
+++ JsAsync/script.js
@@ -1,13 +1,47 @@
-const setTime = function(){
+// const setTime = function(){
+// }
+
+// const changeText = function(){
+//     document.querySelector('h1').innerHTML='Best series'
+// }
+
+// const changeMe = setInterval(changeText, 2000 )
+
+// document.querySelector('#stop').addEventListener('click', function(){
+//     clearTimeout(changeMe)
+//     console.log("STOPPED")
+// })
+
+// ===================================================================
+
+
+
+const manageColor = function(){
+    const hex = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i<6; i++){
+        color += hex[Math.floor(Math.random()*16)]
+        
+    }
+    return color;
+};
+let intervalId
+const startChangetheColor = function(){
+    if(!intervalId){
+        intervalId = setInterval(changeColor,1000)
+    }
+    
+    function changeColor(){
+        document.body.style.backgroundColor = manageColor();
+    }
+    
+};
+
+const stopChangetheColor = function(){
+    clearInterval(intervalId)
+    intervalId =null;
 }
 
-const changeText = function(){
-    document.querySelector('h1').innerHTML='Best series'
-}
+document.querySelector('#start').addEventListener('click', startChangetheColor);
 
-const changeMe = setInterval(changeText, 2000 )
-
-document.querySelector('#stop').addEventListener('click', function(){
-    clearTimeout(changeMe)
-    console.log("STOPPED")
-})
+document.querySelector('#stop').addEventListener('click', stopChangetheColor)
