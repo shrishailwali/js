a8f54b46693d5c2614d9f64e552646aa870956df
SHRISHAIL CHANDRASHEKHAR WALI <114606779+shrishailwali@users.noreply.github.com>
2024-05-27 03:23:05 +0000

    constructor and this keyword

--- 01_classes_and_oops/01_literate.js
+++ 01_classes_and_oops/01_literate.js
@@ -7,5 +7,18 @@ const users = {
     }
 
 }
-console.log(users.password)
-console.log(users.user())
+// console.log(users.password)
+// console.log(users.user())
+
+// ================================constructor function==========================
+function User_(Username, loginCount, isLoggedin){
+    this.Username = Username;
+    this.loginCount = loginCount;
+    this.isLoggedin = isLoggedin;
+    return this
+}
+
+const userOne = new User_("Shrishail",12,true)
+const userTwo = new User_("Shri", 11, false)
+console.log(userOne)
+console.log(userTwo)
