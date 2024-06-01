c46456e6378b8caf5789b8c6934bf1ec5a5a3708
SHRISHAIL CHANDRASHEKHAR WALI <114606779+shrishailwali@users.noreply.github.com>
2024-06-01 13:38:31 +0000

    grocery

--- grocery_store/grocery.js
+++ grocery_store/grocery.js
@@ -1,13 +1,16 @@
 document.addEventListener('click', function(e){
     const input = document.querySelector('#input').value
     const feedback = document.querySelector('#feedback');
+    const ul =document.querySelector('.itemlist')
     if (input ==='' || !isNaN(input)){
         feedback.textContent = `Please Enter a Valid Grocery Item - Correct it "${input}"`;
         feedback.style.color = 'red';
     }
     else{
-        const ul =document.querySelector('itemlist')
-        console.log(ul)
-        ul.textContent = `${input}`
+        feedback.textContent =''
+        const li = document.createElement('li')
+        li.textContent = input
+        ul.append(li)
+        input =''
     }
 })
